dcf8916f28307867223205319d309a93795d6795
liuxiaofeng <1937023747@qq.com>
2020-04-01 00:19:45 +0800

    逆向工程生成pms和sms并前后端调试

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("www.gmall.com");
+        configuration.addAllowedOrigin("gmall.com");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**",configuration);
+        return new CorsWebFilter(configurationSource);
+    }
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SpuDescMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SpuDescEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu信息介绍
+ * 
+ * @author liuxiaofeng
+ * @email xfliu@atguigu.com
+ * @date 2020-03-31 19:58:53
+ */
+@Mapper
+public interface SpuDescMapper extends BaseMapper<SpuDescEntity> {
+	
+}
